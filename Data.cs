f856712b08be3a6e16558a66b567b8e4839eb362
Arindam Kotal <138249469+Arindam03a@users.noreply.github.com>
2025-02-19 23:20:53 +0530

    Create Data.cs

---
+++ Data.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
